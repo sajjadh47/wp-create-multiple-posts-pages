1d64b529ef6e9eb66391c8287cfbb4923e2ee7f7
Sajjad Hossain Sagor <sagorh672@gmail.com>
2025-05-14 16:00:20 +0600

    Update class-wp-create-multi-posts-pages.php

--- includes/class-wp-create-multi-posts-pages.php
+++ includes/class-wp-create-multi-posts-pages.php
@@ -59,7 +59,7 @@ class Wp_Create_Multi_Posts_Pages {
 	 * @access    public
 	 */
 	public function __construct() {
-		$this->version     = defined( 'WP_CREATE_MULTI_POSTS_PAGES_VERSION' ) ? WP_CREATE_MULTI_POSTS_PAGES_VERSION : '1.0.0';
+		$this->version     = defined( 'WP_CREATE_MULTI_POSTS_PAGES_PLUGIN_VERSION' ) ? WP_CREATE_MULTI_POSTS_PAGES_PLUGIN_VERSION : '1.0.0';
 		$this->plugin_name = 'wp-create-multiple-posts-pages';
 
 		$this->load_dependencies();
